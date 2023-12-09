c7c7b9ebca6b7d60d8213d093c167dd99ff782b0
Yogesh <69857209+git-yogeshsharma87@users.noreply.github.com>
2023-12-09 18:56:40 +0000

    variable overview

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const sampleNum = 234324
+let SampleVariable1 = "Hi all"
+var SampleVariable2 = "Hi all second time" 
+SampleVariable3 = "Hi all 3rd time"
+// Never use var 
+SampleVariable1 = "Hi Again"
+SampleVariable2 = "hi"
+SampleVariable3 = 2343
+console.table([SampleVariable1,SampleVariable2,sampleNum,SampleVariable3])
