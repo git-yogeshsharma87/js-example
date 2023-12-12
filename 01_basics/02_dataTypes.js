a8a6dadcd851c686d4251d1fd0626a0e71bab108
Yogesh <69857209+git-yogeshsharma87@users.noreply.github.com>
2023-12-12 11:23:28 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "Yogesh"
+let age = 35
+let isTyping = true
+let state
+
+//number => 2 ^ 53 or something 
+//bigInt
+//string => ""
+// boolean -> true/false 
+//null => standalone value
+//undefined
+// symbol -> unique
+
+// object 
+
+console.log(typeof "Yogesh")
+console.log(typeof null)
+console.log(typeof undefined)
