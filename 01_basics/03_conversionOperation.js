75e0c1fdfae40dfd06fe335df62c5380c225e0a5
Yogesh <69857209+git-yogeshsharma87@users.noreply.github.com>
2023-12-25 14:27:42 +0000

    datatype conversion and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+// ***************************** Operations ********************************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+console.log("1" + value);
+console.log("1" + value + "3");
+console.log("1" + value + 3);
+console.log(+true)
+console.log(-true)
+console.log(+false)
+console.log(-false)
+console.log(+"");
